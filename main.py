fixed the average computation error

main.py
@@ -92,7 +92,7 @@ class main:
         monitors=[]
         monitors_deployment_percentage=[]
 
-        for m_p in [10, 20, 30, 40, 50]:
+        for m_p in [10, 20]:
             monitors_deployment_percentage.append(m_p)
             n=int((m_p/100)*len(G.nodes))
             #self.logger_main.debug(f"m_p {m_p}")
@@ -147,27 +147,27 @@ while(i<n):
     G =mynetwork.creat_topology(topo_type, num_node, degree)
     #mynetwork.tomography_verification(G,'weight')   #here the assigned delay should be 1, place modify the topo.assign_link_delay() function
     optimal_path_selected_percentage_list, avg_diff_of_delay_from_optimal_list, monitors_deployment_percentage =mynetwork.MAB_with_increasing_monitors(G,topo_type,len(G.nodes),degree)
-    #print("n=%d" %(i))
-    #print(optimal_path_selected_percentage_list,avg_diff_of_delay_from_optimal_list)
+
     if i==0:
         multi_times_optimal_path_selected_percentage_array=np.array([optimal_path_selected_percentage_list])
         multi_times_avg_diff_of_delay_from_optimal_array=np.array([avg_diff_of_delay_from_optimal_list])
     else:
-        np.append(multi_times_optimal_path_selected_percentage_array,np.array(optimal_path_selected_percentage_list))
-        np.append(multi_times_avg_diff_of_delay_from_optimal_array,np.array(avg_diff_of_delay_from_optimal_list))
+        mynetwork.logger_main.info(multi_times_optimal_path_selected_percentage_array)
+        multi_times_optimal_path_selected_percentage_array=np.append(multi_times_optimal_path_selected_percentage_array,np.array([optimal_path_selected_percentage_list]), axis=0)
+        multi_times_avg_diff_of_delay_from_optimal_array=np.append(multi_times_avg_diff_of_delay_from_optimal_array,np.array([avg_diff_of_delay_from_optimal_list]), axis=0)
     i += 1
 
 mynetwork.logger_main.info("Statistics:")
-mynetwork.logger_main.info("Before average: percentage of the optimal path selected:")
+mynetwork.logger_main.info("Original: percentage of the optimal path selected:")
 mynetwork.logger_main.info(multi_times_optimal_path_selected_percentage_array)
-mynetwork.logger_main.info("Before average: diff from the real optimal path:")
+mynetwork.logger_main.info("Original: diff from the real optimal path:")
 mynetwork.logger_main.info(multi_times_avg_diff_of_delay_from_optimal_array)
 
 multi_avg_percentage_of_select_optimal_path=np.average(multi_times_optimal_path_selected_percentage_array,axis=0)
 multi_avg_percentage_of_abs_diff_from_optimal=np.average(multi_times_avg_diff_of_delay_from_optimal_array,axis=0)
-mynetwork.logger_main.info("after average: percentage of the optimal path selected:")
+mynetwork.logger_main.info("Average: percentage of the optimal path selected:")
 mynetwork.logger_main.info(multi_avg_percentage_of_select_optimal_path)
-mynetwork.logger_main.info("after average: diff from the real optimal path:")
+mynetwork.logger_main.info("Average: diff from the real optimal path:")
 mynetwork.logger_main.info(multi_avg_percentage_of_abs_diff_from_optimal)
 mynetwork.plotter.plot_optimal_path_selected_percentage_list_with_increasing_monitors(monitors_deployment_percentage, multi_avg_percentage_of_select_optimal_path)
 mynetwork.plotter.plot_abs_diff_path_delay_from_the_optimal(monitors_deployment_percentage,multi_avg_percentage_of_abs_diff_from_optimal )