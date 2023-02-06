e2a14b1bfcd19e45d97be86b07cd13c8fbf4d548
Xu Tao <xutao@Xus-MacBook-Pro.local>
2023-02-06 15:50:09 -0500

    minor changes

--- main.py
+++ main.py
@@ -143,8 +143,8 @@ class main:
         #for m_p in [20, 30]:
             monitors_deployment_percentage.append(m_p)
             n = int((m_p / 100) * len(G.nodes))
-            #if m_p==10:
-            #    n=3
+            if n==2:
+               n=3
             if n <= len(end_nodes):
                 rest_end_nodes = [elem for elem in end_nodes if elem not in monitors]
                 # self.logger_main.debug(f"rest node {rest_end_nodes}")
